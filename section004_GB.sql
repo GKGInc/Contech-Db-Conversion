4d6a4682496cfe8953661a8b60a98a750361715b
Govinda Berrio <gberrio@gkginc.com>
2021-01-07 14:38:56 -0500

    script edits
    - running scripts from context of Contech_Test database
    - added customer conversion script

--- section004_GB.sql
+++ section004_GB.sql
@@ -11,18 +11,88 @@
 -- other:
 --
 
+use Contech_Test
+
 begin transaction
 
-select * into [Contech_Test].dbo.customer
+IF EXISTS(select * from INFORMATION_SCHEMA.tables where TABLE_SCHEMA = 'dbo' and table_name = 'customer')
+    BEGIN
+        drop table dbo.customer
+    END
+GO
 
-alter table [Contech_Test].dbo.customer
-	add customerid int identity
-go
+create table dbo.customer
+(
+    customerid int NOT NULL IDENTITY (1, 1),
+	cust_no char(5) not null,
+	active bit default 0 not null,
+	name char(75) not null,
+	address char(35) default '' not null,
+	address2 char(35) default '' not null,
+	city char(30) default '' not null,
+	state char(3) default '' not null,
+	zip char(11) default '' not null,
+	terms char(10) default '' not null,
+	country char(15) default '' not null,
+	phone char(17) default '' not null,
+	fax char(17) default '' not null,
+	email char(100) default '' not null,
+	memo text default '' not null,
+	vat char(25) default '' not null,
+	b_address char(35) default '' not null,
+	b_address2 char(35) default '' not null,
+	b_city char(30) default '' not null,
+	b_state char(3) default '' not null,
+	b_zip char(11) default '' not null,
+	b_country char(15) default '' not null
+)
+GO
 
-alter table [Contech_Test].dbo.customer
-	add constraint customer_pk
-		primary key nonclustered (customerid)
-go
-
+insert into dbo.customer (
+    [cust_no]
+      ,[active]
+      ,[name]
+      ,[address]
+      ,[address2]
+      ,[city]
+      ,[state]
+      ,[zip]
+      ,[terms]
+      ,[country]
+      ,[phone]
+      ,[fax]
+      ,[email]
+      ,[memo]
+      ,[vat]
+      ,[b_address]
+      ,[b_address2]
+      ,[b_city]
+      ,[b_state]
+      ,[b_zip]
+      ,[b_country]
+)
+SELECT [cust_no]
+      ,[active]
+      ,[name]
+      ,[address]
+      ,[address2]
+      ,[city]
+      ,[state]
+      ,[zip]
+      ,[terms]
+      ,[country]
+      ,[phone]
+      ,[fax]
+      ,[email]
+      ,[memo]
+      ,[vat]
+      ,[b_address]
+      ,[b_address2]
+      ,[b_city]
+      ,[b_state]
+      ,[b_zip]
+      ,[b_country]
+  FROM [rawUpsize_Contech].dbo.[customer]
+GO
 
 commit
