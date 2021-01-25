8d39b71a8f5fc4885002eaeb084c38347553be6c
Helder <hribeiro@gkginc.com>
2021-01-25 16:33:28 -0500

    Section 030 Completed

---
+++ section030_HR.sql
@@ -0,0 +1,250 @@
+-- =========================================================
+-- Section 030: cyclcomp
+-- =========================================================
+
+-- Column changes:
+--  - Set [cyclcompid] to be primary key
+--  - Changed [comp] [char](5) to [componetid] [int] to reference [componet] table
+-- Maps:
+--	- [cyclcomp].[comp]			-- FK = [componet].[comp] --> [componet].[componetid]
+
+USE [Contech_Test]
+
+IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = 'cyclcomp'))
+    DROP TABLE [cyclcomp]
+
+CREATE TABLE [dbo].[cyclcomp](
+	[cyclcompid] [int] IDENTITY(1,1) NOT NULL,
+	[inv_sessid] [char](9) NOT NULL DEFAULT '',
+	--[comp] [char](5) NOT NULL DEFAULT '',
+	[componetid] [int] NOT NULL,		-- FK = [componet].[comp] --> [componet].[componetid]
+	CONSTRAINT [PK_cyclcomp] PRIMARY KEY CLUSTERED 
+	(
+		[cyclcompid] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET IDENTITY_INSERT [cyclcomp] ON;
+
+INSERT INTO [cyclcomp] ([cyclcompid],[inv_sessid],[componetid])
+SELECT [rawUpsize_Contech].[dbo].[cyclcomp].[cyclcompid]
+      ,[rawUpsize_Contech].[dbo].[cyclcomp].[inv_sessid]
+      --,[rawUpsize_Contech].[dbo].[cyclcomp].[comp]
+	  ,ISNULL(componet.[componetid], 0) AS [componetid] 
+  FROM [rawUpsize_Contech].[dbo].[cyclcomp]
+  LEFT JOIN [componet] componet ON [rawUpsize_Contech].[dbo].[cyclcomp].[comp] = componet.[comp] 
+  
+SET IDENTITY_INSERT [cyclcomp] OFF;
+
+--SELECT * FROM [cyclcomp]
+
+-- =========================================================
+-- Section 030: disposit
+-- =========================================================
+
+-- Column changes:
+--  - Added [dispositid] to be primary key
+--  - Changed [price_note] from [text] to [varchar](2000)
+-- Maps:
+--	- [bom_hist].[empnumber]	-- FK = [employee].[empnumber] -> [employee].[employeeid]
+
+USE [Contech_Test]
+
+IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = 'disposit'))
+    DROP TABLE [disposit]
+
+CREATE TABLE [dbo].[disposit](
+	[dispositid] [int] IDENTITY(1,1) NOT NULL,
+	[dispos] [char](2) NOT NULL DEFAULT '',
+	[descript] [char](25) NOT NULL DEFAULT '',
+	CONSTRAINT [PK_disposit] PRIMARY KEY CLUSTERED 
+	(
+		[dispositid] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+INSERT INTO [disposit] ([dispos],[descript])
+SELECT [dispos]
+      ,[descript]
+  FROM [rawUpsize_Contech].[dbo].[disposit]
+  
+--SELECT * FROM [disposit]
+
+-- =========================================================
+-- Section 030: doc_mngr
+-- =========================================================
+
+-- Column changes:
+--  - Set [doc_mngrid] to be primary key
+--  - Changed [add_user] [char](10) to [add_userid] [int] to reference [users] table
+--  - Changed [mod_user] [char](10) to [mod_userid] [int] to reference [users] table
+-- Maps:
+--	- [doc_mngr].[add_user] --> [add_userid]		-- FK = [users].[username] --> [users].[userid]
+--	- [doc_mngr].[add_user] --> [mod_userid]		-- FK = [users].[username] --> [users].[userid]
+
+USE [Contech_Test]
+
+IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = 'doc_mngr'))
+    DROP TABLE [doc_mngr]
+
+CREATE TABLE [dbo].[doc_mngr](
+	[doc_mngrid] [int] IDENTITY(1,1) NOT NULL,
+	[parent_id] [char](15) NOT NULL DEFAULT '', --matlin.matlin_key or corractn.corractnid	? used with parent_tbl (MATLIN, CORRACTN)
+	[parent_tbl] [char](15) NOT NULL DEFAULT '',
+	[document] [char](100) NOT NULL DEFAULT '',
+	[doc_desc] [char](150) NOT NULL DEFAULT '',
+	--[add_user] [char](10) NOT NULL DEFAULT '',
+	[add_userid] [int] NOT NULL DEFAULT 0,				-- FK = [users].[username] --> [users].[userid]
+	[add_dt] [datetime] NULL,
+	--[mod_user] [char](10) NOT NULL DEFAULT '',
+	[mod_userid] [int] NOT NULL DEFAULT 0,				-- FK = [users].[username] --> [users].[userid]
+	[mod_dt] [datetime] NULL,
+	CONSTRAINT [PK_doc_mngr] PRIMARY KEY CLUSTERED 
+	(
+		[doc_mngrid] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET IDENTITY_INSERT [doc_mngr] ON;
+
+INSERT INTO [doc_mngr] ([doc_mngrid],[parent_id],[parent_tbl],[document],[doc_desc],[add_userid],[add_dt],[mod_userid],[mod_dt])
+SELECT [rawUpsize_Contech].[dbo].[doc_mngr].[doc_mngrid]
+      --,[rawUpsize_Contech].[dbo].[doc_mngr].[parent_id]
+	  ,CASE WHEN [parent_tbl] = 'CORRACTN' THEN ISNULL(corractn.[corractnid], 0)	-- corractn.[car_no] --> corractn.[corractnid]
+	  WHEN [parent_tbl] = 'MATLIN' THEN ISNULL(matlin.[matlin_key], 0)
+	  ELSE '' END AS [parent_id]
+      ,[rawUpsize_Contech].[dbo].[doc_mngr].[parent_tbl]
+      ,[rawUpsize_Contech].[dbo].[doc_mngr].[document]
+      ,[rawUpsize_Contech].[dbo].[doc_mngr].[doc_desc]
+      --,[rawUpsize_Contech].[dbo].[doc_mngr].[add_user]
+	  ,ISNULL(add_user.[userid] , 0) as [add_userid]		-- FK = [users].[userid]	
+      ,[rawUpsize_Contech].[dbo].[doc_mngr].[add_dt]
+      --,[rawUpsize_Contech].[dbo].[doc_mngr].[mod_user]
+	  ,ISNULL(mod_user.[userid] , 0) as [mod_userid]		-- FK = [users].[userid]	
+      ,[rawUpsize_Contech].[dbo].[doc_mngr].[mod_dt]
+  FROM [rawUpsize_Contech].[dbo].[doc_mngr]
+  LEFT JOIN [rawUpsize_Contech].[dbo].[matlin] matlin ON [rawUpsize_Contech].[dbo].[doc_mngr].[parent_id] = matlin.[matlin_key]		-- [parent_tbl] = 'CORRACTN'
+  LEFT JOIN [rawUpsize_Contech].[dbo].[corractn] corractn ON [rawUpsize_Contech].[dbo].[doc_mngr].[parent_id] = corractn.[car_no]	-- [parent_tbl] = 'MATLIN'
+  LEFT JOIN [users] add_user ON [rawUpsize_Contech].[dbo].[doc_mngr].[add_user] = add_user.[username]	-- FK = [users].[userid]
+  LEFT JOIN [users] mod_user ON [rawUpsize_Contech].[dbo].[doc_mngr].[mod_user] = mod_user.[username]	-- FK = [users].[userid]
+    
+SET IDENTITY_INSERT [doc_mngr] OFF;
+
+--SELECT * FROM [doc_mngr]
+
+-- =========================================================
+-- Section 030: docs_dtl
+-- =========================================================
+
+-- Column changes:
+--  - Set [docs_dtlid] to be primary key
+--  - Changed [descript] from [text] to [varchar](2000)
+--  - Changed [rev_emp] [char](10) to [rev_employeeid] [int] to reference [employee] table
+-- Maps:
+--	- [docs_dtl].[docs_hdrid]					-- FK = [docs_hdr].[docs_hdrid]
+--	- [docs_dtl].[rev_emp] --> [rev_employeeid]	-- FK = [employee].[empnumber] -> [employee].[employeeid]
+
+USE [Contech_Test]
+
+IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = 'docs_dtl'))
+    DROP TABLE [docs_dtl]
+
+CREATE TABLE [dbo].[docs_dtl](
+	[docs_dtlid] [int] IDENTITY(1,1) NOT NULL,
+	[doctype] [char](4) NOT NULL DEFAULT '',
+	[series] [char](4) NOT NULL DEFAULT '',
+	[extension] [char](20) NOT NULL DEFAULT '',
+	[document] [char](15) NOT NULL DEFAULT '',
+	[descript] [varchar](2000) NOT NULL DEFAULT '',
+	[rev] [char](4) NOT NULL DEFAULT '',
+	[rev_date] [datetime] NULL,
+	[move_date] [datetime] NULL,
+	[docmod] [bit] NOT NULL DEFAULT 0,
+	[docs_hdrid] [int] NOT NULL DEFAULT 0,			-- FK = [docs_hdr].[docs_hdrid]
+	[cpmr_rev] [char](4) NOT NULL DEFAULT '',
+	[cpmr_date] [datetime] NULL,
+	[rev_rec] [int] NOT NULL DEFAULT 0,
+	[rev_dt] [datetime] NULL,
+	--[rev_emp] [char](10) NOT NULL DEFAULT '',
+	[rev_employeeid] [int] NOT NULL DEFAULT 0,		-- FK = [employee].[empnumber] -> [employee].[employeeid]
+	CONSTRAINT [PK_docs_dtl] PRIMARY KEY CLUSTERED 
+	(
+		[docs_dtlid] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET IDENTITY_INSERT [docs_dtl] ON;
+
+INSERT INTO [docs_dtl] ([docs_dtlid],[doctype],[series],[extension],[document],[descript],[rev],[rev_date],[move_date],[docmod],[docs_hdrid],[cpmr_rev],[cpmr_date],[rev_rec],[rev_dt],[rev_employeeid])
+SELECT [rawUpsize_Contech].[dbo].[docs_dtl].[docs_dtlid]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[doctype]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[series]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[extension]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[document]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[descript]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[rev]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[rev_date]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[move_date]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[docmod]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[docs_hdrid]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[cpmr_rev]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[cpmr_date]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[rev_rec]
+      ,[rawUpsize_Contech].[dbo].[docs_dtl].[rev_dt]
+      --,[rawUpsize_Contech].[dbo].[docs_dtl].[rev_emp]
+	  ,ISNULL(employee.[employeeid], 0) AS [employeeid]	-- FK = [employee].[empnumber] -> [employee].[employeeid]
+  FROM [rawUpsize_Contech].[dbo].[docs_dtl]
+  LEFT JOIN [employee] employee ON [rawUpsize_Contech].[dbo].[docs_dtl].[rev_emp] = employee.[empnumber]	
+  
+SET IDENTITY_INSERT [docs_dtl] OFF;
+
+--SELECT * FROM [docs_dtl]
+
+-- =========================================================
+-- Section 030: docs_hdr
+-- =========================================================
+
+-- Column changes:
+--  - Set [docs_hdrid] to be primary key
+--  - Changed [doctype] [char](4) to [doctypeid] [int] to reference [docs_dtl] table
+-- Maps:
+--	- [docs_hdr].[doctype] --> [doctypeid]	-- FK = [doctypes].[doctype] --> [doctypes].[doctypeid]
+
+USE [Contech_Test]
+
+IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = 'docs_hdr'))
+    DROP TABLE [docs_hdr]
+
+CREATE TABLE [dbo].[docs_hdr](
+	[docs_hdrid] [int] IDENTITY(1,1) NOT NULL,
+	--[doctype] [char](4) NOT NULL DEFAULT '',		-- FK = [doctypes].[doctype]
+	[doctypeid] [int] NOT NULL DEFAULT 0,			-- FK = [doctypes].[doctype] -> [doctypes].[doctypeid]
+	[series] [char](4) NOT NULL DEFAULT '',
+	[descript] [char](75) NOT NULL DEFAULT '',
+	CONSTRAINT [PK_docs_hdr] PRIMARY KEY CLUSTERED 
+	(
+		[docs_hdrid] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET IDENTITY_INSERT [docs_hdr] ON;
+
+INSERT INTO [docs_hdr] ([docs_hdrid],[doctypeid],[series],[descript])
+SELECT [rawUpsize_Contech].[dbo].[docs_hdr].[docs_hdrid]
+      --,[rawUpsize_Contech].[dbo].[docs_hdr].[doctype]
+	  ,ISNULL(doctypes.[doctypeid], 0) AS [docs_dtlid] -- FK = [doctypes].[doctype] -> [doctypes].[doctypeid]
+      ,[rawUpsize_Contech].[dbo].[docs_hdr].[series]
+      ,[rawUpsize_Contech].[dbo].[docs_hdr].[descript]
+  FROM [rawUpsize_Contech].[dbo].[docs_hdr]
+  LEFT JOIN [doctypes] doctypes ON [rawUpsize_Contech].[dbo].[docs_hdr].[doctype] = doctypes.[doctype]
+  
+SET IDENTITY_INSERT [docs_hdr] OFF;
+
+--SELECT * FROM [docs_hdr]
+
+-- =========================================================
